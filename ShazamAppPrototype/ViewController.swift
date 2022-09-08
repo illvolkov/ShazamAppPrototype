52040261a9ff47bbda71016ed25f9f06a3adfc9f
Ilya Volkov <illvolkov@bk.ru>
2022-09-08 22:00:26 +0500

    Initial Commit

---
+++ ShazamAppPrototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShazamAppPrototype
+//
+//  Created by Ilya Volkov on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
